2b386cc04f3e1cb286d9f6a48cc9dee35e22d2e2
Raúl Camilo Martín <rcmartinb@correo.udistrital.edu.co>
2020-08-24 22:58:10 -0500

    Geoestadistics :heavy_check_mark:

--- Projects/Geostatistics/KS_KO.R
+++ Projects/Geostatistics/KS_KO.R
@@ -36,15 +36,14 @@ plot(E,N)
 #no normalidad  entonces anamorfosis gaussiana
 library(RGeostats)
 library(gstat)
-data<-cbind(E, N, Tmt)
+data<-cbind(N, E, Tmt)
 temp<-as.data.frame(data)
 #01. Fit Gaussian Anamorphosis 
-mdb = temp[,c("E","N","Tmt")] #must be 3 columns: x,y,z
+mdb = temp[,c("N","E","Tmt")] #must be 3 columns: x,y,z
 mdb.rgdb = db.create(mdb,ndim=2,autoname=F)
 mdb.herm = anam.fit(mdb.rgdb,name="Tmt",type="gaus")
 mdb.hermtrans = anam.z2y(mdb.rgdb,names="Tmt",anam=mdb.herm)
 Tmt.trans = mdb.hermtrans@items$Gaussian.Tmt 
-
 
 
 library(gstat)
@@ -59,13 +58,7 @@ names(borde) <- c("E","N")
 coordinates(borde) = ~E+N
 # m = vgm(0.6,"Exp",35000, 0.4)
 detach("package:sgeostat")
-# 
-# zn.svgm <- variogram(Tmt.trans~1,temp2s)
-# vgm1 = vgm(0.544,"Exp",19000, 0.01)
-# plot(zn.svgm,m)
 
-
-# m <- vgm1
 m = vgm(2.1,"Exp",32000, 0.02)
 # Kriging Ordinario
 pron.pts.ko<-krige(Tmt.trans~1,temp2s,borde,model=m) 
@@ -76,32 +69,26 @@ ko.tempdbvar = anam.y2z(ko.bts.db,names="var1.var",anam = mdb.herm)
 min(ko.tempdb@items$Raw.var1.pred)
 max(ko.tempdb@items$Raw.var1.pred)
 mean(ko.tempdb@items$Raw.var1.pred)
-summary(ko.tempdbvar@items$R)
+summary(ko.tempdbvar@items$var1.var)
 #Prediction map
 grilla_ko<-grillabaser[,52:53]
 grilla_ko$pred<-ko.tempdb@items$Raw.var1.pred
 grilla_ko$var<-ko.tempdbvar@items$var1.var
 names(grilla_ko) <- c("E","N","pre","var")
 coordinates(grilla_ko)= ~E+N
-gridded(grilla_ko)=T
 x11()
-spplot(grilla_ko, "pre", main="Temperatura media terrestre \nPredicciones Kriging Ordinario", col.regions=bpy.colors(150), cuts=15, cex.main=0.2, scales = list(draw =T), xlab="Este (m)", ylab = "Norte (m)",  key.space=list(space="right", cex=0.6))
+spplot(grilla_ko, "pre", main="Precipitación Media Anual \nPredicciones Kriging Ordinario", col.regions=bpy.colors(150), cuts=15, cex.main=0.2, scales = list(draw =T), xlab="Este (m)", ylab = "Norte (m)",  key.space=list(space="right", cex=0.6))
 
-spplot(grilla_ko, "var", main="Temperatura media terrestre \nVarianzas Kriging Ordinario", col.regions=bpy.colors(150), cuts=15, cex.main=0.2, scales = list(draw =T), xlab="Este (m)", ylab = "Norte (m)",  key.space=list(space="right", cex=0.6))
+spplot(grilla_ko, "var", main="Precipitación Media Anual \nVarianzas Kriging Ordinario", col.regions=bpy.colors(150), cuts=15, cex.main=0.2, scales = list(draw =T), xlab="Este (m)", ylab = "Norte (m)",  key.space=list(space="right", cex=0.6))
 
 typeof(pron.pts.ko)
 # Kriging Simple
-
-ms = vgm (2.1,"Exp",44000, 0.01)
-pron.pts.ks<-krige(Tmt.trans~1,temp2s,borde,model=ms,beta=mu) 
+pron.pts.ks<-krige(Tmt.trans~1,temp2s,borde,model=m,beta=mu) 
 
 ks.bts = cbind(coordinates(pron.pts.ks),pron.pts.ks@data)
 ks.bts.db = db.create(ks.bts,autoname = F)
 ks.tempdb = anam.y2z(ks.bts.db,names="var1.pred",anam = mdb.herm)
 ks.tempdbvar = anam.y2z(ks.bts.db,names="var1.var",anam = mdb.herm)
-min(ks.tempdb@items$Raw.var1.pred)
-max(ks.tempdb@items$Raw.var1.pred)
-mean(ks.tempdb@items$Raw.var1.pred)
 min(ks.tempdbvar@items$var1.var)
 max(ks.tempdbvar@items$var1.var)
 mea
@@ -113,17 +100,15 @@ grilla_ks$pred<-ks.tempdb@items$Raw.var1.pred
 grilla_ks$var<-ks.tempdbvar@items$var1.var
 names(grilla_ks) <- c("E","N","pre","var")
 coordinates(grilla_ks)= ~E+N
-gridded(grilla_ks)=T
 x11()
-spplot(grilla_ks, "pre", main="Temperatura media terrestre\nPredicciones Kriging Simple", col.regions=bpy.colors(150), cuts=15, cex.main=0.2, scales = list(draw =T), xlab="Este (m)", ylab = "Norte (m)",  key.space=list(space="right", cex=0.6))
+spplot(grilla_ks, "pre", main="Precipitación Media Anual \nPredicciones Kriging Simple", col.regions=bpy.colors(150), cuts=15, cex.main=0.2, scales = list(draw =T), xlab="Este (m)", ylab = "Norte (m)",  key.space=list(space="right", cex=0.6))
 
-spplot(grilla_ks, "var", main="Temperatura media terrestre \nVarianza Kriging Simple", col.regions=bpy.colors(150), cuts=15, cex.main=0.2, scales = list(draw =T), xlab="Este (m)", ylab = "Norte (m)",  key.space=list(space="right", cex=0.6))
+spplot(grilla_ks, "var", main="Precipitación Media Anual \nVarianza Kriging Simple", col.regions=bpy.colors(150), cuts=15, cex.main=0.2, scales = list(draw =T), xlab="Este (m)", ylab = "Norte (m)",  key.space=list(space="right", cex=0.6))
 
 
 KO.esf.cv.z <- krige.cv(Tmt.trans~1,temp2s,borde,model=m)     
-KS.esf.cv.z <- krige.cv(Tmt.trans~1,temp2s,borde,model=ms,beta=mu) 
+KS.esf.cv.z <- krige.cv(Tmt.trans~1,temp2s,borde,model=m,beta=mu) 
 
-Tmt<
 # Validación
 
 resultados.cv.z <- rbind(criterio.cv(KO.esf.cv.z), criterio.cv(KS.esf.cv.z))
@@ -141,12 +126,11 @@ criterio.cv(x)
 
 
 # contorno del mapa de croacia 
-df.pts<-spsample(IDSTA.OV, n=9, type="regular") 
+df.pts<-spsample(IDSTA.OV, n=25500, type="regular") 
 loc1 <- df.pts@coords 
 colnames(loc1) <- c("x","y")
 plot(IDSTA.OV)
-x11()
-plot(loc1)
+points(loc1)
 
 library(maptools)
 Polygon <- readShapePoly("D:/Documentos/11 semestre/Geoestadística/ProyectoGeoestadistica/original_data/croatia.shp")
